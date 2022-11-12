ca4ab797cb673804768953eb19892a101098444f
tilda <iamtheworst@programmer.net>
2022-11-12 16:15:57 -0600

    add cog loading because i forgot gg

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from ruamel.yaml import YAML
 from logbook import Logger, StreamHandler, INFO
 from logbook.compat import redirect_logging
 from sys import stdout
+import os
 
 redirect_logging()
 StreamHandler(stdout, level=INFO).push_application
@@ -20,5 +21,19 @@ bot = HiyorinBot(
     intents=intents,
     command_prefix='hiyorin '
 )
+
+if __name__ == "__main__":
+    for ext in os.listdir("cogs"):
+        if ext.endswith(".py"):
+            try:
+                log.info(f"attempting to load {ext}")
+                ext = ext.replace(".py", "")
+                bot.load_extension(f"ext.{ext}")
+            except Exception:
+                log.error(f"failed to load {ext}", exc_info=True)
+            else:
+                log.info(f"successfully loaded {ext}")
+    log.info("loading jishaku")
+    bot.load_extension("jishaku")
 
 bot.run(config['bot']['token'])

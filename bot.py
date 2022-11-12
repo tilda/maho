060e914caeb6e111d907d429b79427bcd9d0a022
tilda <iamtheworst@programmer.net>
2022-11-12 02:42:54 -0600

    the

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@ from discord.ext import commands
 import discord
 from core.bot import HiyorinBot
 from ruamel.yaml import YAML
+from logbook import Logger, StreamHandler, INFO
+from logbook.compat import redirect_logging
+
+redirect_logging()
+StreamHandler(stdout, level=INFO).push_application
+log = Logger("hiyorin")
 
 yaml = YAML()
 config = yaml.load(open('config.yaml').read())
 bot = HiyorinBot(
     config=config,
+    logger=log,
     intents=discord.Intents.default(),
     command_prefix='hiyorin '
 )

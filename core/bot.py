d0f8440d55d4baaf906316eee31b2d8665dca567
tilda <iamtheworst@programmer.net>
2022-11-12 18:55:37 -0600

    fix home base copy

--- core/bot.py
+++ core/bot.py
@@ -2,13 +2,13 @@ from discord.ext import commands
 from aiohttp import ClientSession
 import discord
 from transmission_rpc import Client as TransmissionClient
-
 class HiyorinBot(commands.Bot):
     def __init__(self, config, logger, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.log = logger
         self.http_session = ClientSession()
         self.config = config
+        self.home_base = discord.Object(id=self.config['bot']['home_base_id'])
         self.transmission_client = TransmissionClient(
             host=self.config['transmission']['host'],
             port=self.config['transmission']['port'],
@@ -19,5 +19,5 @@ class HiyorinBot(commands.Bot):
     async def setup_hook(self):
         # Copy global commands to home base and sync, so new commands
         # are added immediately
-        self.tree.copy_global_to(guild=self.config['bot']['home_base_id'])
-        await self.tree.sync(self.config['bot']['home_base_id'])
+        self.tree.copy_global_to(guild=self.home_base)
+        await self.tree.sync(guild=self.home_base)

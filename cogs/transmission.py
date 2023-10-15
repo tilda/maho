18c9a142d174c235025550043b2dec5cc93cee93
tilda <iamtheworst@programmer.net>
2023-10-15 03:52:45 -0500

    a???

--- cogs/transmission.py
+++ cogs/transmission.py
@@ -1,6 +1,8 @@
+from typing import Optional
 from core.cog import Cog
 import discord
 from discord import app_commands
+import asyncio
 
 class Transmission(Cog):
     def __init__(self, bot):
@@ -8,6 +10,21 @@ class Transmission(Cog):
         pass
     
     @app_commands.command()
+    @app_commands.check(Cog.is_owner)
     @app_commands.describe(torrent="A .torrent file to upload to Transmission")
-    async def queue_torrent(self, ctx: discord.Interaction, torrent: discord.Asset):
-        return
+    async def queue_torrent(self, ctx: discord.Interaction, torrent_file: discord.Attachment, label: Optional[str]):
+        progress: discord.Message = await ctx.response.send_message('Got it, sending to Transmission.')
+        
+        try:
+            async with asyncio.timeout(10):
+                await self.bot.loop.run_in_executor(
+                    None,
+                    self.bot.transmission_client.add_torrent(
+                        torrent=torrent_file.url,
+                        labels=label
+                    )
+                )
+        except TimeoutError:
+            return await progress.edit(content='Request timed out. Did something catch on fire?')
+        else:
+            return await progress.edit(content=f'Successfully added {torrent_file.filename} to Transmission.')
--- core/cog.py
+++ core/cog.py
@@ -1,10 +1,18 @@
 from discord.ext import commands
+from discord.app_commands import check
+from discord import Interaction
+from core.bot import MahoBot
 
 class Cog(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: MahoBot = bot
         super().__init__(bot)
     
     @property
     def config(self):
         return self.bot.config
+    
+    @check
+    @staticmethod
+    def is_owner(ctx: Interaction):
+        return ctx.client.is_owner(ctx.user.id)
